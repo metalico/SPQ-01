d717b84f0b5945d3c6b65c3e969f2c5d4a71ddca
I. Usobiaga <inaki.usobiag@opendeusto.es>
2016-05-06 10:00:02 +0200

    Estructura y funcionamiento deJUnit arreglados.

---
+++ src/test/java/es/deusto/spq/TestMoverNave.java
@@ -0,0 +1,34 @@
+package test.java.es.deusto.spq;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import main.java.es.deusto.spq.windows.VentanaJuego;
+
+public class TestMoverNave {
+	
+	VentanaJuego v;
+	
+	@Before
+	public void setUp() throws Exception {
+		v=new VentanaJuego("aoe");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void testMoverIzquierda() {
+		v.moverIzquierda();
+		assertTrue(VentanaJuego.nv.getPista()==2);
+	}
+
+	@Test
+	public void testMoverDerecha() {
+		v.moverDerecha();
+		assertTrue(VentanaJuego.nv.getPista()==4);
+	}
+}
